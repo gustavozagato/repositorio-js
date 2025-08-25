2264acc0b0b887a50a17d76d227dba81a25f3c03
Gustavo Zagato <gustavozagatobottechia@gmail.com>
2025-08-25 16:22:55 -0300

    dados

--- exercicios/ex3/script.js
+++ exercicios/ex3/script.js
@@ -1,47 +1,50 @@
 var dado = document.getElementById('dado')
 var numero = document.getElementById('numero')
-var sortear = document.getElementById('sorteio')
+let nmax = 4;
+
+function sortearNumero(max) {
+    return Math.floor(Math.random() * max) + 1;
+
+}
 
 function D4() {
     dado.src = 'imagens/d4.png'
-    var n = 4;
+    nmax = 4;
 }
 
 function D6() {
     dado.src = 'imagens/d6.png'
-    var n = 6;
+    nmax = 6;
 }
 
 function D8() {
     dado.src = 'imagens/d8.png'
-    var n = 8;
+    nmax = 8;
 }
 
 function D10() {
-    dado.src = 'imagens/d4.png'
-    var n = 10;
+    dado.src = 'imagens/d10.png'
+    nmax = 10;
 }
 
 function D12() {
-    dado.src = 'imagens/d4.png'
-    var n = 12;
+    dado.src = 'imagens/d12.png'
+    nmax = 12;
 }
 
 function D20() {
-    dado.src = 'imagens/d4.png'
-    var n = 20;
+    dado.src = 'imagens/d20.png'
+    nmax = 20;
 }
 
 function D100() {
-    dado.src = 'imagens/d4.png'
-    var n = 100;
+    dado.src = 'imagens/d100.png'
+    nmax = 100;
 }
 
-function sortearNumero(max) {
-    return Math.floor(Math.random() * (max - 0 + 1)) + 0;
-
+function resultado() {
+    let numeroSorteado = sortearNumero(nmax);
+    numero.innerText = numeroSorteado;
 }
-var numeroSorteado = sortearNumero(n);
-numero.innerText(numero)
 
 
